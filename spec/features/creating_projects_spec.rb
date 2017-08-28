6e9fd626d17d18d837414fe9df6d77ae9ac9c1ee
TheWolf91 <the_wolf91@libero.it>
2017-08-28 18:42:00 +0200

    Added admin flag to User model, and seeded the first admin user

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Users can create new projects" do
   before do
+    login_as(FactoryGirl.create(:user, :admin))
     visit "/"
 
     click_link "New Project"
   end
+
   scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 3"
     fill_in "Description", with: "A text editor for everyone"
