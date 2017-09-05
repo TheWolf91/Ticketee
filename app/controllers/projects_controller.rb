3d82ee37e03fe8b4427e93f10be7865886066ead
TheWolf91 <the_wolf91@libero.it>
2017-09-05 16:33:28 +0200

    Only admins and managers of a project can edit the project's details

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = policy_scope(Project)
   end
 
   def show
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-
+    authorize @project, :update?
   end
 
   def update
